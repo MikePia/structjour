649b76e56a0569fe5eca90351fc2cda04d2cdd56
MikePia <lynnpete11@gmail.com>
2018-09-23 18:47:16 -0600

    TheTradeObject.py is the focus of this commit. It correctly creates the summary for one trade as tested in TheTradeObject.ipynb.

--- withstyle/thetradeobject.py
+++ withstyle/thetradeobject.py
@@ -9,17 +9,134 @@
 # all the visuals present.
 
 import pandas as pd
-import datetime.timedelta
+import datetime
 # from journalfiles import JournalFiles
 from structjour.pandasutil import DataFrameUtil 
 # InputDataFrame, ToCSV_Ticket as Ticket
 from structjour.tradeutil import  FinReqCol
 # XLImage, TradeUtil
 
+# Use to access columns in the (altered) input dataframe, known on this page as df. Use srf (SumReqFields instance) to access
+# columns/data for the summary trade dataframe, known on this page as TheTrade.
 frc = FinReqCol()
 
+class SumReqFields(object):
+    '''Manage the required columns, cell location and namedStyle for the summary aka TheTradeObject and TheTradeStyle'''
+
+    def __init__(self):
+        rcvals =['Name', 'Account', 'P / L', 'Start', 'Duration', 'Strategy', 'StratNote', 'Shares', 'MktVal', 
+                 'Link1', 'Link2', 'Link3', 'Link4', 'Link5', 'Link6', 
+                 'EntryHead', 'Entry1', 'Entry2', 'Entry3', 'Entry4', 'Entry5', 
+                 'ExitHead', 'Exit1', 'Exit2', 'Exit3', 'Exit4', 'Exit5', 
+                 'TargHead', 'Target', 'TargDiff', 
+                 'StopHead', 'StopLoss', 'SLDiff', 
+                 'RRHead', 'RR', 'MaxHead', 'MaxLoss', 'Explain', 'Notes']
+        
+        rckeys = ['name', 'acct', 'PL', 'start', 'dur', 'strat', 'stratnote', 'shares', 'mktval', 
+                  'link1', 'link2', 'link3', 'link4', 'link5', 'link6',
+                  'entryhead', 'entry1', 'entry2', 'entry3', 'entry4', 'entry5',
+                  'exithead', 'exit1', 'exit2', 'exit3', 'exit4', 'exit5',
+                  'targhead', 'targ', 'targdiff',
+                  'stophead', 'stoploss', 'sldiff',
+                  'rrhead', 'rr', 'maxhead', 'maxloss', 'explain', 'notes']
+        
+        rc = dict(zip(rckeys, rcvals))
+        
+        # The user should generally use these as dot accessors to get the columns names. <soapbox> I am against strings as the primary navigation
+        # in a programming language. Leave that to SQL in VB or html. A level of abstraction is safer. rc.name rather than rc['Name']
+        # Dictionary type is for programmers/implementation, not users/interface. </soapbox>
+        self.name     = rc['name']
+        self.acct      = rc['acct']
+        self.PL       = rc['PL']
+        self.start    = rc['start']
+        self.dur      = rc['dur']
+        self.strat    = rc['strat']
+        self.stratnote= rc['stratnote']
+        self.shares   = rc['shares']
+        self.mktval   = rc['mktval']
+        self.link1    = rc['link1']
+        self.link2    = rc['link2']
+        self.link3    = rc['link3']
+        self.link4    = rc['link4']
+        self.link5    = rc['link5']
+        self.link6    = rc['link6']
+        self.entryhead= rc['entryhead']
+        self.entry1   = rc['entry1']
+        self.entry2   = rc['entry2']
+        self.entry3   = rc['entry3']
+        self.entry4   = rc['entry4']
+        self.entry5   = rc['entry5']
+        self.exithead = rc['exithead']
+        self.exit1    = rc['exit1']
+        self.exit2    = rc['exit2']
+        self.exit3    = rc['exit3']
+        self.exit4    = rc['exit4']
+        self.exit5    = rc['exit5']
+        self.targhead = rc['targhead']
+        self.targ     = rc['targ']
+        self.targdiff = rc['targdiff']
+        self.stophead = rc['stophead']
+        self.stoploss = rc['stoploss']
+        self.sldiff   = rc['sldiff']
+        self.rrhead   = rc['rrhead']
+        self.rr       = rc['rr']
+        self.maxhead  = rc['maxhead']
+        self.maxloss  = rc['maxloss']
+        self.explain  = rc['explain']
+        self.notes    = rc['notes']
+        
+        self.rc = rc
+        self.columns = rc.values()
+        
+        self.tfcolumns = { 
+            self.name     : [[(1, 1), (3, 2)],'titleStyle'],
+            self.acct      : [[(4 ,1), (6, 2)],'titleStyle'],
+            self.PL       : [[(7, 1), (9, 2)],'titleStyle'],
+            self.start    : [[(1, 3), (3, 4)],'titleStyle'],
+            self.dur      : [[(4, 3), (6, 4)],'titleStyle'],
+            self.strat    : [[(7, 3), (9, 4)],'titleStyle'],
+            self.stratnote: [[(7, 5), (9, 6)],'titleStyleStratNotes'],
+            self.shares   : [[(7, 7), (9, 8)],'titleStyle'],
+            self.mktval   : [[(7, 9), (9, 10)],'titleStyle'],
+            self.link1    : [[(7, 11),(9, 12)],'titleStyleLink'],
+            self.link2    : [[(7, 13),(9, 14)],'titleStyleLink'],
+            self.link3    : [[(7, 15),(9, 16)],'titleStyleLink'],
+            self.link4    : [[(7, 17),(9, 18)],'titleStyleLink'],
+            self.link5    : [[(7, 19),(9, 20)],'titleStyleLink'],
+            self.link6    : [[(7, 21),(9, 22)],'titleStyleLink'],
+            self.entryhead: [(1, 5),'normStyle'],
+            self.entry1   : [(2, 5),'normStyle'],
+            self.entry2   : [(3, 5),'normStyle'],
+            self.entry3   : [(4, 5),'normStyle'],
+            self.entry4   : [(5, 5),'normStyle'],
+            self.entry5   : [(6, 5),'normStyle'],
+            self.exithead : [(1, 6),'normStyle'],
+            self.exit1    : [(2, 6),'normStyle'],
+            self.exit2    : [(3, 6),'normStyle'],
+            self.exit3    : [(4, 6),'normStyle'],
+            self.exit4    : [(5, 6),'normStyle'],
+            self.exit5    : [(6, 6),'normStyle'],
+            self.targhead : [(1, 7),'normStyle'],
+            self.targ     : [(2, 7),'normStyle'],
+            self.targdiff : [(3, 7),'normStyle'],
+            self.stophead : [(1, 8),'normStyle'],
+            self.stoploss : [(2, 8),'normStyle'],
+            self.sldiff   : [(3, 8),'normStyle'],
+            self.rrhead   : [(1, 9),'normStyle'],
+            self.rr       : [(2, 9),'normStyleRR'],
+            self.maxhead  : [(1, 10),'normStyle'],
+            self.maxloss  : [(2, 10),'normStyle'],
+            self.explain  : [[(1,11), (6,16)],'noteStyle'],
+            self.notes    : [[(1,17), (2,22)],'noteStyle']
+    
+                     }
+            #TODO get a list of namd styles and verify that all of these strings are on the list. Come up with a mechanism to make this configurable by the user
+        
+# global variable for use in this module    
+srf=SumReqFields()
 class TheTradeObject(object):
     '''Summarize one trade at a time'''
+
 
     def __init__(self, df):
         '''
@@ -28,49 +145,10 @@ class TheTradeObject(object):
         :params:df: A DataFrame that includes the transactions, or tickets, from a singel trade.
         '''
         
-        tfcolumns = { 
-            frc.name   : [(1, 1), (3, 2 )],
-            frc.acct   : [(4 ,1), (6, 2)],
-            frc.PL     : [(7, 1), (9, 2)],
-            frc.start  : [(1, 3), (3, 4)],
-            frc.dur    : [(4, 3), (6, 4)],
-            "Strategy" : [(7, 3), (9, 4)],
-            "StratNote": [(7, 5), (9, 6)],
-            "Shares"   : [(7, 7), (9, 8)],
-            "MktVal"   : [(7, 9), (9, 10)],
-            "Link1"    : [(7, 11),(9, 12)],
-            "Link2"    : [(7, 13),(9, 14)],
-            "Link3"    : [(7, 15),(9, 16)],
-            "Link4"    : [(7, 17),(9, 18)],
-            "Link5"    : [(7, 19),(9, 20)],
-            "Link6"    : [(7, 21),(9, 22)],
-            "EntriesHeader": (1, 5),
-            "Entries1"     : (2, 5),
-            "Entries2"     : (3, 5),
-            "Entries3"     : (4, 5),
-            "Entries4"     : (5, 5),
-            "Entries5"     : (6, 5),
-            "ExitsHeader"  : (1, 6),
-            "Exits1"       : (2, 6),
-            "Exits2"       : (3, 6),
-            "Exits3"       : (4, 6),
-            "Exits4"       : (5, 6),
-            "Exits5"       : (6, 6),
-            "TargHead"     : (1, 7),
-            "Target"       : (2, 7),
-            "TargDiff"     : (3, 7),
-            "StopHead"     : (1, 8),
-            "StopLoss"     : (2, 8),
-            "SLDiff"       : (3, 8),
-            "RRHead"       : (1, 9),
-            "RR"           : (2, 9),
-            "MaxHead"      : (1, 10),
-            "MaxLoss"      : (2, 10),
-            "Explain"  : [(1,11), (6,16)],
-            "Notes"    : [(1,17), (2,22)]
-    
-                     }
-        TheTrade = pd.DataFrame(columns=tfcolumns.keys())
+        
+        
+        col=srf.tfcolumns.keys()
+        TheTrade = pd.DataFrame(columns=col)
         TheTrade = DataFrameUtil.addRows(TheTrade, 1)
 
         ix = df.index[-1]
@@ -78,9 +156,9 @@ class TheTradeObject(object):
 
         
         #TODO This list should be retrieved from TheStrategyObject
-        strats = ["ORB", "ABCD", "VWAP Reversal", "Bull Flag", "Fallen ANgel",
-          "VWAP False Breakout", "VWAP Reversal", "15 Minute Reversal",
-          "VWAP MA trend", "Other", "Skip"]
+        strats = ['ORB', 'ABCD', 'VWAP Reversal', 'Bull Flag', 'Fallen Angel',
+          'VWAP False Breakout', 'VWAP Reversal', '15 Minute Reversal',
+          'VWAP MA trend', 'Other', 'Skip']
         side = df.loc[ix0][frc.side]
         self.df = df
         self.TheTrade = TheTrade
@@ -90,22 +168,47 @@ class TheTradeObject(object):
         self.side = side
         self.shares = 0
         
-    def _setName(self):
-        self.TheTrade[frc.name]=self.df.loc[self.ix][frc.name]
+    def runSummary(self):
+        self.__setName()
+        self.__setAcct()
+        self.__setSum()
+        self.__setStart()
+        self.__setDur()
+        self.__setMarketValue()
+        
+        # Place items requireing user input in seperate method
+        self.__setStrategy()            # Goes in Different method
+        self.__setShares()
+        self.__setHeaders()
+        self.__setEntries()
+        self.__setTarget()              # Goes in Different method
+        self.__setStop()         # Goes in Different method
+        self.__setMaxLoss()
+        return self.__setRiskReward()
+    
 
-    def _setAcct(self):
-        self.TheTrade[frc.acct]="Live" if self.df.loc[self.ix][frc.acct].startswith("U") else "SIM"
+        
+    def __setName(self):
+        self.TheTrade[srf.name]=self.df.loc[self.ix][frc.name]
+        return self.TheTrade
 
-    def _setSum(self):
-        self.TheTrade[frc.PL]=self.df.loc[self.ix][frc.sum]
+    def __setAcct(self):
+        self.TheTrade[srf.acct]='Live' if self.df.loc[self.ix][frc.acct].startswith('U') else 'SIM'
+        return self.TheTrade
 
-    def _setStart(self):
-        self.TheTrade[frc.start]=self.df.loc[self.ix][frc.start]
+    def __setSum(self):
+        self.TheTrade[srf.PL]=self.df.loc[self.ix][frc.sum]
+        return self.TheTrade
 
-    def _setDur(self):
+    def __setStart(self):
+        self.TheTrade[srf.start]=self.df.loc[self.ix][frc.start]
+        return self.TheTrade
+
+    def __setDur(self):
         time = self.df.loc[self.ix][frc.dur]
         duration = "{0} hours {1}:{2}".format(time.seconds // 3600, time.seconds // 60, time.seconds % 60)
-        self.TheTrade[frc.dur]=duration
+        self.TheTrade[srf.dur]=duration
+        return self.TheTrade
 
     def getStrategy(self) :
 
@@ -128,48 +231,49 @@ class TheTradeObject(object):
                 continue
         return ireply -1
 
-    def _setStrategy(self):
+    def __setStrategy(self):
         reply = self.getStrategy()
     
 
         if reply == 9 :
             response = input("What do you want to call the strategy?")
-            self.TheTrade.Strategy = response
+            self.TheTrade[srf.strat] = response
         elif reply == 10 :
             pass
         elif reply > -1 and reply < len(self.strats):
-            self.TheTrade.Strategy=self.strats[reply]
+            self.TheTrade[srf.strat] =self.strats[reply]
         else :
             print("WTF?  reply out of bounds. 'reply' = {0}".format( reply))
             raise ValueError
+        return self.TheTrade
         
-    def _getShares(self):
+    def __getShares(self):
         if self.shares == 0 :
             if self.side.startswith("B"):
                 self.shares = self.df[frc.bal].max()
             else :
                 self.shares = self.df[frc.bal].min()
         return self.shares
+            
+    def __setShares(self):
+        self.TheTrade.Shares = "{0} shares".format(self.__getShares())
+        return self.TheTrade
         
-        
-    def _setShares(self):
-        self.TheTrade.Shares = "{0} shares".format(self._getShares())
-        
-    def _setMarketValue(self):
-        mkt = self._getShares() * self.df.loc[self.ix0][frc.price]
+    def __setMarketValue(self):
+        mkt = self.__getShares() * self.df.loc[self.ix0][frc.price]
         self.TheTrade.MktVal   = mkt
+        return self.TheTrade
 
-    def _setHeaders(self):
-        self.TheTrade['EntriesHeader'] = "Entries"
-        self.TheTrade['ExitsHeader'] = "Exits"
-        self.TheTrade.TargHead = "Target"
-        self.TheTrade.StopHead = "Stop"
-        self.TheTrade.RRHead = "R:R"
-        self.TheTrade.MaxHead = "Max Loss"
-
-    
-    
-    def _setEntries(self):
+    def __setHeaders(self):
+        self.TheTrade[srf.entryhead] = 'Entries'
+        self.TheTrade[srf.exithead] = 'Exits'
+        self.TheTrade[srf.targhead] = 'Target'
+        self.TheTrade[srf.stophead] = 'Stop'
+        self.TheTrade[srf.rrhead] = 'R:R'
+        self.TheTrade[srf.maxhead] = 'Max Loss'
+        return self.TheTrade[[srf.entryhead, srf.exithead, srf.targhead, srf.stophead, srf.rrhead, srf.maxhead]]
+  
+    def __setEntries(self):
         B=list()
         S=list()
         
@@ -181,9 +285,9 @@ class TheTradeObject(object):
     
         if len(B) > 5 :
             more = len(B) - 5
-            self.TheTrade.Entries5 =  "Plus {} more.".format(more)
+            self.TheTrade.Entry5 =  "Plus {} more.".format(more)
         for i, price in zip(range (len(B)), B) :
-            col = "Entries" + str(i+1)
+            col = "Entry" + str(i+1)
             self.TheTrade[col] = price
     
 
@@ -191,10 +295,12 @@ class TheTradeObject(object):
             more = len(S) - 5
             self.TheTrade.Exits5 =  "Plus {} more.".format(more)
         for i, price in zip(range (len(S)), S) :
-            col = "Exits" + str(i+1)
+            col = "Exit" + str(i+1)
             self.TheTrade[col] = price
+        return self.TheTrade[[srf.entryhead, srf.entry1, srf.entry2, srf.entry3, srf.entry4, srf.entry5, 
+                              srf.exithead, srf.exit1, srf.exit2, srf.exit3, srf.exit4, srf.exit5]]
     
-    def _setTarget(self) :
+    def __setTarget(self) :
         target = 0
         while True:
             try :
@@ -211,10 +317,11 @@ class TheTradeObject(object):
                 
                 continue
             break
-        self.TheTrade.Target = target
-        self.TheTrade.TargDiff = target - self.TheTrade.Entries1
+        self.TheTrade[srf.targ] = target
+        self.TheTrade[srf.targdiff] = target - self.TheTrade[srf.entry1]
+        return self.TheTrade
 
-    def _setStop(self) :
+    def __setStop(self) :
         stop = 0
         while True:
             try :
@@ -229,18 +336,21 @@ class TheTradeObject(object):
                 ''')
                 continue
             break
-        self.TheTrade.StopLoss = stop
-        self.TheTrade.SLDiff = stop - self.TheTrade.Entries1
+        self.TheTrade[srf.stoploss] = stop
+        self.TheTrade[srf.sldiff] = stop - self.TheTrade[srf.entry1]
+        return self.TheTrade
     
-    def _setMaxLoss(self):
+    def __setMaxLoss(self):
         #TODO write some tests that check for unknown types in the SLDiff col. Then fix this to handle them.
         
-        self.TheTrade.MaxLoss = self.TheTrade.SLDiff * self._getShares()
+        self.TheTrade.MaxLoss = self.TheTrade.SLDiff * self.__getShares()
+        return self.TheTrade
 
-    def _getRiskReward(self):
+    def __setRiskReward(self):
         #Write some tests that check for unknown types and then handle them here
                 
         self.TheTrade.RR = self.TheTrade.StopLoss / self.TheTrade.Target
+        return self.TheTrade
         
 
 
