f8ae4139f3c48ff21b14f2c1b1b52b922c1a9df1
MikePia <lynnpete11@gmail.com>
2019-01-03 11:57:04 -0700

    Qt stuff is incomplete. Just sit until chart stuff is implemented.

--- src/trade.py
+++ src/trade.py
@@ -2,7 +2,10 @@
 @author: Mike Petersen
 Top level module currently.
 '''
-import datetime
+# import datetime
+import sys
+
+from PyQt5.QtWidgets import QApplication
 
 from journalfiles import JournalFiles
 from journal.pandasutil import InputDataFrame, ToCSV_Ticket as Ticket
@@ -10,11 +13,12 @@ from journal.tradeutil import TradeUtil
 from withstyle.layoutsheet import LayoutSheet
 from withstyle.tradestyle import TradeFormat
 from withstyle.mstksum import MistakeSummary
+from withstyle.qtform import QtForm
 # pylint: disable=C0103
 
-# jf=JournalFiles(theDate=datetime.date(2018,10,26), mydevel=True)
+#jf=JournalFiles(theDate=datetime.date(2018,12,3), mydevel=True)
 
-jf = JournalFiles(mydevel=True)
+jf = JournalFiles(mydevel=True, outdir='out')
 jf.printValues()
 
 tkt = Ticket(jf)
@@ -27,7 +31,7 @@ trades = idf.processInputFile(trades)
 tu = TradeUtil()
 inputlen, dframe, ldf = tu.processOutputDframe(trades)
 
-# Process the openpyxl excel object using the output file DataFrame. Insert 
+# Process the openpyxl excel object using the output file DataFrame. Insert
 # images and Trade Summaries.
 sumSize = 25
 margin = 25
@@ -48,6 +52,13 @@ mistake.mstkSumStyle(ws, tf, mstkAnchor)
 mistake.dailySumStyle(ws, tf, ldf, mstkAnchor)
 
 tradeSummaries = ls.createSummaries(imageLocation, ldf, jf, ws, tf)
+app = QApplication(sys.argv)
+qtf = QtForm()
+# qtf.fillForm(tradeSummaries[2])
+# app.exec_()
+print("moving past")
+
+
 ls.createMistakeForm(tradeSummaries, mistake, ws, imageLocation)
 ls.createDailySummaryForm(tradeSummaries, mistake, ws, mstkAnchor)
 
