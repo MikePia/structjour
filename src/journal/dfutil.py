6b27bb73f9319ce5fee7f7d6f240aa6ce770ecdb
MikePia <lynnpete11@gmail.com>
2019-02-08 12:38:03 -0700

    createDf() will now take list of DataFrame as a parameter.

--- src/journal/dfutil.py
+++ src/journal/dfutil.py
@@ -5,6 +5,7 @@ Created on Oct 19, 2018
 '''
 
 import pandas as pd
+# pylint: disable=C0103
 
 
 class DataFrameUtil(object):
@@ -13,65 +14,65 @@ class DataFrameUtil(object):
     without regard to instance.
     '''
 
-
     def __init__(self, params):
         '''
         Constructor which may never be used. (but it could be)
         '''
 
     @classmethod
-    def checkRequiredInputFields(cls, dframe, requiredFields) :
+    def checkRequiredInputFields(cls, dframe, requiredFields):
         '''
         Checks that dframe has the fields in the array requiredFields. Also checks that there are no duplicate fields
         Returns True on success. Raises a ValueError on failure.
         :param:dframe:          The DataFrame that is being checked.
         :param:requiredFields:  The fields that are required. 
         '''
-        actualFields=dframe.columns
-        if len(actualFields) != len(set(actualFields)) :
+        actualFields = dframe.columns
+        if len(actualFields) != len(set(actualFields)):
             err = 'Your DataFrame has duplicate columns'
             raise(ValueError(err))
-        if set (requiredFields) <= (set (actualFields)) :
+        if set(requiredFields) <= (set(actualFields)):
             return True
-        else :
-            err='Your DataFrame is missing some required fields:\n'
+        else:
+            err = 'Your DataFrame is missing some required fields:\n'
             err += str((set(requiredFields) - set(actualFields)))
             raise ValueError(err)
-    
-    
-    #TODO Change this to create the DataFrame from an array of fields
+
     @classmethod
-    def createDf(cls, dframe, numRow) :
-        ''' 
-        Creates a new DataFrame with  the length numRow. Each cell is filled with empty string 
-        
-        :param dframe:  (Will be) ... An array of the labels to create columns.
-        :param numRow:  The number of empty rows to create. 
-        :return:                   The new DataFrame objet
+    def createDf(cls, cols, numRow, fill=''):
         '''
-    
-        ll=list()
-        r=list()
-        for _ in range(len(dframe.columns)) :
-            r.append('')
-            
-        for _ in range(numRow) :
+        Creates a new DataFrame with the length numRow. Each cell is filled with empty string
+        :param cols:  An array or DataFrame to use as the column headers.
+        :param numRow:  The number of empty rows to create.
+        :params fill: Each cell will be filled with fill.
+        :return:        The new DataFrame objet
+        '''
+
+        ll = list()
+        r = list()
+
+        if isinstance(cols, type(pd.DataFrame())):
+            cols = cols.columns
+        for _ in range(len(cols)):
+            r.append(fill)
+
+        for _ in range(numRow):
             ll.append(r)
-        newdf= pd.DataFrame(ll, columns=dframe.columns)
-    
+        newdf = pd.DataFrame(ll, columns=cols)
+
         return newdf
-    
+
     @classmethod
-    def addRows(cls, dframe, numRow) :
+    def addRows(cls, dframe, numRow, fill=''):
         ''' 
-        Adds numRow rows the the DataFrame object dframe'
-        
-        :param:dframe:            The DataFrame to increase in size 
-        :param:requiredFields:    The number of empty rows to create. 
-        :return:                   The new DataFrame objet
+        Adds numRow rows to the end of the DataFrame object dframe'
+        :params dframe: A DataFrame to increase in size.
+        :params numRow: he number of empty rows to create.
+        :params fill: Each cell wil be filled with fill.
+        :return: The new DataFrame objet
         '''
-        
-        newdf= cls.createDf(dframe, numRow)
+
+        newdf = cls.createDf(dframe, numRow, fill)
         dframe = dframe.append(newdf, ignore_index=True, sort=False)
-        
+
         return dframe
