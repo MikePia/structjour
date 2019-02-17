ca91c2604024cfec07b3192dc7f5adc3356294b9
MikePia <lynnpete11@gmail.com>
2019-02-17 13:25:24 -0700

    Added and updated some tests. Adjusted XLImage parameters.

--- src/journal/xlimage.py
+++ src/journal/xlimage.py
@@ -4,6 +4,8 @@ Created on Oct 16, 2018
 @author: Mike Petersen
 '''
 import os
+import unittest
+
 from PIL import Image as PILImage
 from PIL import ImageGrab
 from openpyxl.drawing.image import Image
@@ -18,37 +20,47 @@ def askUser(question):
     return response
 
 class XLImage(object):
+    '''Handle image stuff'''
 
 #     20.238095238095237
+    def __init__(self, default='data/defaultImage.png', heightInCells=20, pixPerCell=19.3):
+        '''
+        Create the XLImage, set the name and the size image here.
+        :params default: Location of a default image to use
+        :params heightInCells: Determine the height based on how many excel rows for the height.
+        :params pixPerCell: The aproximate number of pixels per excel row.  Because this is
+                            determined by issues beyond our control, it will (eventually) be
+                            configurable in user settings. The number of pixels is unscientifically
+                            determined (by me) to be about 19.3.
 
-    def adjustSizeByHeight(self, sz, newHeight=425, numCells=0, pixPerCell = 19.3) :
+        '''
+        self.defaultImage = default
+        self.numCells = heightInCells
+        self.pixPerCell = pixPerCell
+
+
+    def adjustSizeByHeight(self, sz):
         ''' 
-        Adjust size to keep the aspect ratio the same as determined by newHeight
-        :param:sz: A tuple of ints in the form (width, height).
-        :param:newHeight: The height in pixels of the image as place in the excel doc. The default number is
-                            based on 21 unaltered excel cells on my machine.
-        :param:numCells: An alternate way to determine the height based on how many excel rows for the height.
-                            This value will override newHeight. The number of pixels is unscientifically determined 
-                            (by me) to be about 20.24.
-        :param:pixPerCell: The aproximate number of pixels per excel row.  Because this is determined by issues
-                            beyond our control, it will have to be configurable in user settings     
+        Adjust size to keep the aspect ratio the same as determined by self.numCells and 
+        self.pixPerCell
         '''
         #TODO Make some contraption to adjust the size by the user. 
-        w,h = sz
-        if numCells > 0:
-            newHeight = numCells * pixPerCell
+        w, h = sz
+
+        newHeight = self.numCells * self.pixPerCell
         newWidth = newHeight * w/h
-        nw=int(newWidth)
-        nh=int(newHeight)
+        nw = int(newWidth)
+        nh = int(newHeight)
         return(nw, nh)    
 
     def getDefaultPILImage(self):
-        im = PILImage.open("data/defaultImage.png")
+        '''Return a default image'''
+        im = PILImage.open(self.defaultImage)
         return im
 
     def getPilImageFromClipboard(self, msg):
         '''
-        Ask the user if the image is ready and collect the response
+        Retrieve the image from the clipboard. Ask the user if the image is ready and collect the response
         Use the PIL library to get an image from the clipboard. The ImageGrab.grabclibboard() works on
         Windows and MAC only. On failure to get an image, give the user 4 more tries or the user can opt out by
         entering 'q'.
@@ -57,34 +69,32 @@ class XLImage(object):
         :return:     The image in as a PIL object or None
         '''
 
-        for i in range (5):
-            msg_go = "{0} {1}".format(msg, "Are you ready? (q to skip image) \n\t\t\t\t")
+        for i in range(5):
+            msg_go = "{0} {1}".format(msg, "Are you ready? (n to skip image) \n\t\t\t\t")
             response = askUser(msg_go)
             im = None
-            if response.lower().startswith('y') or response == '': 
+            if response.lower().startswith('y') or response == '':
                 im = ImageGrab.grabclipboard()
-            elif response.lower().startswith('q') :
+            elif response.lower().startswith('q') or response.lower().startswith('n'):
                 # return self.getDefaultPILImage()
                 return None
-            if im is None :
+            elif response.lower().startswith('d'):
+                return self.getDefaultPILImage()
+
+            if im is None:
                 print("Failed to get an image. Please select and copy an image (or press 'q')")
-            else :
+            else:
                 return im
         print("Moving on")
         return self.getDefaultPILImage()
-    
-    
-    
-    
+
     def getAndResizeImage (self, name, outdir) :
         '''
-        A script to run the image manipulation in structjour. Request a clipboard copy of an image. 
-        Resze it to newSize height. Save it with a new name. Return the name. Hackiness lives until 
-        I figure how to create a proper openpyxl Image object from a PIL Image object that doesn't 
-        make the Workbook puke. (OTOH Saving the image is kind of a nice touch as long as the name 
-        has enough information to make the file useful ... like (Trade1_TWTR-Short_930-3min.jpeg)
-        :param:name: An original name. We mark it up with _resize, save it, and return the new name.
-        :param:outdir: The location to save the images.
+        Tell the user to copy an image to the clipboard then call getPilImageFromClipboard. A
+        script to run the image manipulation in structjour. Save it with a new name. Return the
+        name. 
+        :params name: The name to save the image.
+        :param outdir: The location to save the images.
         :return: The pathname of the image we save.
         '''
 
@@ -98,7 +108,7 @@ class XLImage(object):
             pilImage = self.getPilImageFromClipboard(msg)
             if not pilImage:
                 return None
-            newSize = self.adjustSizeByHeight(pilImage.size, numCells=20)
+            newSize = self.adjustSizeByHeight(pilImage.size)
             pilImage = pilImage.resize(newSize, PILImage.ANTIALIAS)
 
             resizeName, ext = self.getResizeName(name, outdir)
---
+++ src/test/test_xlimage.py
@@ -0,0 +1,91 @@
+'''
+Test the methods and functions in journal.xlimage
+Created on Feb 17, 2019
+
+@author: Mike Petersen
+'''
+import datetime as dt
+import os
+import random
+import unittest
+
+# from PIL import Image as PILImage
+# from PIL import ImageGrab
+# from openpyxl.drawing.image import Image
+
+from journal.xlimage import XLImage
+# pylint: disable = C0103
+
+
+
+class TestXLImage(unittest.TestCase):
+    '''
+    Test the functions in methods in thetradeobject module
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super(TestXLImage, self).__init__(*args, **kwargs)
+        # global DD
+
+        ddiirr = os.path.dirname(__file__)
+        os.chdir(os.path.realpath(ddiirr + '/../'))
+
+    def test_getDefault(self):
+        '''
+        Test the method journal.xlimage.XLImage.getDefaultPILImage. Specifically test that it opens
+        an image whether an image is specified.
+        '''
+        img = r'C:\python\E\structjour\src\data\defaultImage.png'
+        os.path.exists(img)
+
+        xli = XLImage(img)
+
+        im = xli.getDefaultPILImage()
+        self.assertEqual(im.format.lower(), 'png')
+        xli2 = XLImage()
+        im2 = xli2.getDefaultPILImage()
+        self.assertGreater(im2.format, 1)
+
+
+    def test_AdjustSizeByHeight(self):
+        '''
+       Test the method XLImage.adjustSizeByHeight
+        '''
+        xl = XLImage()
+        random.seed(dt.datetime.now())
+
+        pixDelta = 0.02
+        #Test input tuple
+        for dummy in range(10):
+            insz = (random.randint(200, 2500), random.randint(200, 2500))
+            w, h = xl.adjustSizeByHeight(insz)
+            self.assertAlmostEqual(insz[0]/insz[1], w/h, delta=pixDelta)
+
+        #Test Figuring by cell number
+        for dummy in range(10):
+            insz = (random.randint(200, 2500), random.randint(200, 2500))
+            nc = (random.randint(15, 35))
+            xl = XLImage(heightInCells=nc)
+            w, h = xl.adjustSizeByHeight(insz)
+            self.assertAlmostEqual(insz[0]/insz[1], w/h, delta=pixDelta)
+
+        #test changing pixPerCell
+        for dummy in range(10):
+            insz = (random.randint(200, 2500), random.randint(200, 2500))
+            ppc = (random.uniform(30, 35))
+            nc = (random.randint(15, 35))
+            xl = XLImage(heightInCells=nc, pixPerCell=ppc)
+            w, h = xl.adjustSizeByHeight(insz)
+            self.assertAlmostEqual(insz[0]/insz[1], w/h, delta=pixDelta)
+
+
+
+
+def notmain():
+    '''run some local code'''
+    t = TestXLImage()
+    # t.test_getDefault()
+    t.test_AdjustSizeByHeight()
+
+if __name__ == '__main__':
+    notmain()
