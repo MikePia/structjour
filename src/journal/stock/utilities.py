3132545a118ea534d99c149a08cd9d6dab88efca
MikePia <lynnpete11@gmail.com>
2019-05-08 13:48:28 -0600

    Placed the api key in db and  connected port stff from settings

--- src/journal/stock/utilities.py
+++ src/journal/stock/utilities.py
@@ -5,9 +5,12 @@ Local utility functions shared by some stock modules and test code.
 
 @creation_date: 2019-01-17
 '''
-# pylint: disable = C0103
+import os
 import datetime as dt
+import sqlite3
+
 from PyQt5.QtCore import QSettings
+# pylint: disable = C0103
 
 def getLastWorkDay(d=None):
     '''
@@ -43,6 +46,107 @@ def getPrevTuesWed(td):
     before = td - dt.timedelta(deltdays)
     return before
 
+class ManageKeys:
+    def __init__(self, create=False, db=None):
+        self.settings = QSettings('zero_substance', 'structjour') 
+        self.apiset = QSettings('zero_substance/stockapi', 'structjour')
+        self.db = db
+        if not self.db:
+            self.setDB()
+        
+        if create and self.db:
+            self.createTables()
+
+
+    def setDB(self, db=None):
+        '''
+        Set the location of the sqlite db file. Its placed in the apiset but could belong in
+        settings. The db does not seem to have a natural place to be excluded from.
+        Arbitrarily, its in apiset only. (apiset 'belongs' to the stockapi, but the db has a range
+        of things beyond that).
+        '''
+        o = self.settings.value('journal')
+        if not o:
+            msg = '\nWARNING: Trying to set the db location.'
+            msg = msg +  '\nPlease set the location of your journal directory.\n'
+            print(msg)
+            return
+        self.db = os.path.join(o, 'structjour.sqlite')
+        self.apiset.setValue('dbsqlite', self.db)
+
+        if not os.path.exists(self.db):
+            msg = 'No db listed-- do we recreate the default and add a setting?- or maybe pop and get the db address'
+            # self.createTables()
+            # raise ValueError(msg)
+    
+    def createTables(self):
+        '''
+        Creates the api_keys if it doesnt exist then adds a row for each api that requires a key
+        if they dont exist
+        '''
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+
+        cur.execute('''
+            CREATE TABLE if not exists api_keys (
+            id	INTEGER PRIMARY KEY AUTOINCREMENT,
+            api	TEXT NOT NULL UNIQUE,
+            key	TEXT);''')
+        conn.commit()
+
+        cur.execute('''
+            SELECT api from api_keys WHERE api = ?;''', ("bc",))
+
+        cursor = cur.fetchone()
+        if not cursor:
+            cur.execute('''
+                INSERT INTO api_keys(api)VALUES(?);''', ("bc",))
+
+        cur.execute('''
+            SELECT api from api_keys WHERE api = ?;''', ("av",))
+
+        cursor = cur.fetchone()
+        if not cursor:
+            cur.execute('''
+                INSERT INTO api_keys(api)VALUES(?);''', ("av",))
+        conn.commit()
+
+    def updateKey(self, api, key):
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+
+        cur.execute('''UPDATE api_keys
+            SET key = ?
+            WHERE api = ?''', (key, api))
+        conn.commit()
+
+    def getKey(self, api):
+        if not self.db:
+            return
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+
+        cur.execute('''SELECT key
+            FROM api_keys
+            WHERE api = ?''', (api, ))
+        k = cur.fetchone()
+        if k:
+            return k[0]
+        return k
+
+
+
+ 
+
+
+    def getDB(self):
+        '''Get the file location of the sqlite database'''
+        db = self.apiset.value('dbsqlite')
+        if not db:
+            print('WARNING: Trying to retrieve db location, the database file location is not set.')
+        return db
+
+
 class IbSettings:
     def __init__(self):
         self.apiset = QSettings('zero_substance/stockapi', 'structjour')
@@ -61,6 +165,8 @@ class IbSettings:
             ibreal = self.apiset.value('ibRealCb', False, bool)
             ibPaper = self.apiset.value('ibPaperCb', False, bool)
             ibpref = self.apiset.value('ibPref')
+            self.ibPort = None
+            self.ibId = None
             if ibreal:
                 self.ibPort = self.apiset.value('ibRealPort', 7496, int)
                 self.ibId = self.apiset.value('ibRealId', 7878, int)
@@ -70,6 +176,8 @@ class IbSettings:
 
     def getIbSettings(self):
         #TODO abstrast host like port and id-- set it in the stockapi dialog
+        if not self.ibPort or not self.ibId:
+            return None
         d = {'port': self.ibPort,
                 'id': self.ibId,
                 'host': '127.0.0.1'}
@@ -78,13 +186,28 @@ class IbSettings:
 
 
 def notmain():
-    '''run some local code'''
-    now = dt.datetime.today()
-    fmt = "%A, %B %d   %w"
+    mk = ManageKeys(create=True, db='C:\\python\\E\\structjour\\src\\test\\testdb.sqlite')
+    mk.updateKey('bc', '''That'll do pig''')
+    mk.updateKey('av', '''That'll do.''')
+    print(mk.getKey('bc'))
+    print(mk.getKey('av'))
+
+def localstuff():
+    settings = QSettings('zero_substance', 'structjour') 
+    apiset = QSettings('zero_substance/stockapi', 'structjour')
+    setkeys = settings.allKeys()
+    apikeys = apiset.allKeys()
+    setval=list()
+    apival=list()
+    for k in setkeys:
+        setval.append(settings.value(k))
+    for k in apikeys:
+        apival.append(apiset.value(k))
+
     print()
-    for i in range(7):
-        d = now - dt.timedelta(i)
-        print(f'{d.strftime(fmt)} ... : ... {getLastWorkDay(d).strftime(fmt)}')
-        print(d.isoweekday())
+
 if __name__ == '__main__':
-    notmain()
+    # notmain()
+    localstuff()
+
+
---
+++ src/test/test_utilities.py
@@ -0,0 +1,219 @@
+'''
+@author: Mike Petersen
+
+@creation_date: 2019-01-17
+'''
+import datetime as dt
+import os
+import pickle
+import types
+import unittest
+
+from PyQt5.QtCore import QSettings
+
+from journal.stock import utilities as util
+# pylint: disable = C0103
+
+
+class PickleSettings:
+    '''A utility to store and restore settings for use in testing. Be careful not to lose data.'''
+    def __init__(self):
+        ddiirr = os.path.dirname(__file__)
+        os.chdir(os.path.realpath(ddiirr))
+        self.settings = QSettings('zero_substance', 'structjour') 
+        self.apiset = QSettings('zero_substance/stockapi', 'structjour')
+        self.setkeys = []
+        self.setvals = []
+        self.apisetkeys = []
+        self.apisetvals = []
+
+        self.name = os.path.join(ddiirr, 'pickleset.zst')
+        print(self.name)
+
+    def initializeSettings(self):
+        self.settings.clear()
+        self.apiset.clear()
+
+    def removePickle(self):
+        if os.path.exists(self.name):
+            os.remove(self.name)
+
+    def initializeVars(self):
+        self.setkeys = []
+        self.setvals = []
+        self.apisetkeys = []
+        self.apisetvals = []
+
+
+    def storeSettings(self, replacePickle=False):
+        if os.path.exists(self.name):
+            if not replacePickle:
+                return
+        self.initializeVars()
+        self.setkeys = self.settings.allKeys()
+        for k in self.setkeys:
+            self.setvals.append(self.settings.value(k))
+
+        self.apisetkeys = self.apiset.allKeys()
+        for k in self.apisetkeys:
+            self.apisetvals.append(self.apiset.value(k))
+
+        setsnkeys = [self.setkeys, self.setvals, self.apisetkeys, self.apisetvals]
+
+        with open(self.name, "wb") as f:
+            '''Cannot pickle qsettings objects- so we pickle a list'''
+            pickle.dump((setsnkeys), f)
+
+    def restoreSettings(self):
+        if os.path.exists(self.name):
+            with open(self.name, "rb") as f:
+                setsnkeys = pickle.load(f)
+                for k, v in zip(setsnkeys[0], setsnkeys[1]):
+                    self.settings.setValue(k, v)
+            
+                for k2, v2 in zip(setsnkeys[2], setsnkeys[3]):
+                    self.apiset.setValue(k2, v2)
+
+            os.remove(self.name)
+        else:
+            print(f'No pickle found at {self.name}')
+
+class TestUtilities(unittest.TestCase):
+    '''Test functions in the stock.utilities module'''
+
+    def __init__(self,  *args, **kwargs):
+        super(TestUtilities, self).__init__(*args, **kwargs)
+        ddiirr = os.path.dirname(__file__)
+        os.chdir(os.path.realpath(ddiirr))
+        self.p = os.path.realpath(ddiirr)
+
+    def test_getLastWorkDay(self):
+        '''run some local code'''
+        now = dt.datetime.today()
+        fmt = "%a, %B %d"
+        
+        print()
+        for i in range(7):
+            d = now - dt.timedelta(i)
+            dd = util.getLastWorkDay(d)
+
+            print(f'{d.strftime(fmt)} ... : ... {util.getLastWorkDay(d).strftime(fmt)}')
+            self.assertTrue(dd.isoweekday() < 6)
+            self.assertTrue(dd.isoweekday() > 0)
+
+    def test_setDB(self):
+        '''
+        Test the method ManageKeys.setDB. The location depends on having the journal location set.
+        This method is called by init y default.
+        '''
+        t = PickleSettings()
+        t.storeSettings()
+        t.initializeSettings()
+        settings = QSettings('zero_substance', 'structjour')
+        apiset = QSettings('zero_substance/stockapi', 'structjour')
+
+        settings.setValue('journal', self.p)
+        mk = util.ManageKeys(create=True)
+        l = apiset.value('dbsqlite')
+        self.assertTrue(os.path.exists(l))
+        os.remove(l)
+
+        t.initializeSettings()
+        settings.setValue('journal', self.p)
+        mk = util.ManageKeys(create=True)
+        ll = apiset.value('dbsqlite')
+        self.assertTrue(l == ll)
+        os.remove(ll)
+        # self.assertEqual(l, ll)
+
+        t.initializeSettings()
+        # settings.setValue('journal', self.p)
+        mk = util.ManageKeys(create=True)
+        lll = apiset.value('dbsqlite')
+        self.assertFalse(lll)
+
+        t.restoreSettings()
+        print(apiset.allKeys())
+        print(settings.allKeys())
+        # self.assertTrue(os)
+        print()
+
+    def test_updateKey(self):
+        t = PickleSettings()
+        t.storeSettings()
+        t.initializeSettings()
+        settings = QSettings('zero_substance', 'structjour')
+        apiset = QSettings('zero_substance/stockapi', 'structjour')
+
+        settings.setValue('journal', self.p)
+        mk = util.ManageKeys(create=True)
+        mk.updateKey('bc', 'Its the end of the world')
+        mk.updateKey('av', 'as we know it')
+        bck = mk.getKey('bc')
+        avk = mk.getKey('av')
+        self.assertTrue(bck == 'Its the end of the world')
+        self.assertTrue(avk == 'as we know it')
+
+        l = apiset.value('dbsqlite')
+
+        t.restoreSettings()
+        print(apiset.allKeys())
+        print(settings.allKeys())
+        os.remove(l)
+
+        # self.assertTrue(os)
+        mk = util.ManageKeys()
+        print(mk.getKey('bc'))
+
+    def test_ibSettings(self):
+        t = PickleSettings()
+        t.storeSettings()
+        t.initializeSettings()
+
+        apiset = QSettings('zero_substance/stockapi', 'structjour')
+        apiset.setValue('ibRealCb', True)
+        apiset.setValue('ibPaperCb', False)
+        ibs = util.IbSettings()
+        defport = 7496
+        defid = 7878
+        defhost = '127.0.0.1'
+        p = ibs.getIbSettings()
+        self.assertEqual(defhost, p['host'])
+        self.assertEqual(defid, p['id'])
+        self.assertEqual(defport, p['port'])
+
+        t.restoreSettings()
+        print(apiset.allKeys())
+
+
+def notmain():
+    '''Run some local code'''
+    # t = TestUtilities()
+    # t.test_getLastWorkDay()
+    # t.test_setDB()
+    # t.test_updateKey()
+    # t.test_ibSettings()
+    # t = PickleSettings()
+    # t.storeSettings()
+    # t.initializeSettings()
+    # t.restoreSettings()
+    # t.removePickle()
+    t = PickleSettings()
+    # t.storeSettings()
+    t.initializeSettings()
+
+def main():
+    '''
+    test discovery is not working in vscode. Use this for debugging. Then run cl python -m unittest
+    discovery
+    '''
+    f = TestUtilities()
+    for name in dir(f):
+        if name.startswith('test'):
+            attr = getattr(f, name)
+            if isinstance(attr, types.MethodType):
+                attr()
+
+if __name__ == '__main__':
+    notmain()
+    # main()
