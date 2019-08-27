731abdf6d9f8d59e4e472831b33d544e15fd3233
MikePia <lynnpete11@gmail.com>
2019-08-27 01:06:32 -0600

    Open any file and get statement type

---
+++ src/journal/statements/statement.py
@@ -0,0 +1,117 @@
+# Structjour -- a daily trade review helper
+# Copyright (C) 2019 Zero Substance Trading
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+'''
+Module to open any file and return the right statement type
+
+@author: Mike Petersen
+@creation_data: 08/26/19
+'''
+import os
+import pandas as pd
+import re
+
+from bs4 import BeautifulSoup
+
+from PyQt5.QtCore import QSettings
+from journal.dfutil import DataFrameUtil
+
+from journal.statements.findfiles import findFilesSinceMonth
+from journal.statements.dasstatement import DasStatement
+from journal.statements.findfiles import getDirectory
+from journal.statements.ibstatement import IbStatement, readit
+from journal.definetrades import ReqCol
+
+
+def getStatement(infile):
+    '''
+    Determine if infile is a statement. If it is return a tuple (data, type)
+    If it is a DAS statement, determine if it matches the current date. As DAS statements do not
+    include dates, the date in structjour and the directory structure date must match.
+    If they don't match, the program, at a higher level, will pop a query to get the date of the
+    statement.
+    '''
+    file, ext = os.path.splitext(infile)
+    if not os.path.exists(infile) or (
+        ext.lower() != '.csv' and not ext.lower().startswith('.htm')):
+        return None, None
+    if ext == '.csv':
+        df = pd.read_csv(infile, names=[x for x in range(0, 100)])
+        if df.iloc[0][0] == 'BOF'  or df.iloc[0][0] == 'HEADER' or (
+                df.iloc[0][0] == 'ClientAccountID') or (
+                df.iloc[0][0] == 'Statement'):
+            return df, "IB_CVS"
+        df = pd.read_csv(infile)
+        if not df.empty:
+            requiredFields = list(ReqCol().columns)
+            requiredFields.remove('Date')
+
+            # A small hack to allow tradesByTickets to pass as a DAS export
+            if not 'PnL' in df.columns:
+                requiredFields.remove('PnL')
+                requiredFields.append('P / L')
+            try:
+                if DataFrameUtil.checkRequiredInputFields(df, requiredFields):
+                    if not checkDateDir(infile):
+                        return None, 'DAS'
+                    return df, 'DAS'
+            except ValueError:
+                pass
+
+    elif ext.lower().startswith('.htm'):
+        soup = BeautifulSoup(readit(infile), 'html.parser')
+        tbldivs = soup.find_all("div", id=lambda x: x and x.startswith('tbl'))
+        if tbldivs:
+            return tbldivs, 'IB_HTML'
+    return None, None
+
+def checkDateDir(infile, frmt=4444):
+    '''
+    Get the date from the path
+    '''
+    settings = QSettings('zero_substance', 'structjour')
+    directory = getDirectory()
+    indir = os.path.split(infile)
+    if not os.path.normpath(directory) == os.path.normpath(indir[0]):
+        return False
+    return True
+    
+
+
+
+
+
+
+def notmain():
+    d = '20190601'
+    fs = findFilesSinceMonth(d, '', searchParts=True, DAS=True)
+    for f, dd in fs:
+        x, inputtype = getStatement(f)
+        print(f, inputtype)
+        if inputtype == 'DAS':
+            if x is None:
+                print ('     We got mismatching dates here')
+    
+
+
+if __name__ == '__main__':
+    notmain()
+
+
+
+
+    
+
