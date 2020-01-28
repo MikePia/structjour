eac8f937a278b05c6feb1b61c1239dcd14e7a179
MikePia <lynnpete11@gmail.com>
2020-01-27 18:17:07 -0700

    Dividing tests between unit and system tests- in process

--- test/test_dailycontrol.py
+++ test/test_dailycontrol.py
@@ -21,232 +21,132 @@ Created on May 22, 2019
 
 @author: Mike Petersen
 '''
-import logging
-import os
 import sys
 import unittest
-from  unittest import TestCase
-
-import pandas as pd
 import sqlite3
+from unittest import TestCase
+import pandas as pd
 
-# https://stackoverflow.com/questions/57426219/how-to-import-qtwebenginewidgets-after-qapplication-has-been-created
-# thanks to ekhumoro.
-# Strangely the import error occurs using (before installing webengine_hack here):
-# python -m unittest discover
-# But not when using from the test directory:
-# python -m unittest test_dailycontrol
-# or any other local run
-def webengine_hack():
-    from PyQt5 import QtWidgets
-    app = QtWidgets.QApplication.instance()
-    if app is not None:
-        import sip
-        app.quit()
-        sip.delete(app)
-    import sys
-    from PyQt5 import QtCore, QtWebEngineWidgets
-    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
-    app = QtWidgets.qApp = QtWidgets.QApplication(sys.argv)
-    return app
-
-try:
-    # just for testing
-    from PyQt5 import QtWidgets
-    app = QtWidgets.QApplication([''])
-    from PyQt5 import QtWebEngineWidgets
-except ImportError as exception:
-    logging.info('\nRetrying webengine import...')
-    app = webengine_hack()
-    from PyQt5 import QtWebEngineWidgets
 
 # from PyQt5 import QtCore, QtWebEngineWidgets
-from PyQt5.QtCore import Qt, QSettings
-from PyQt5.QtWidgets import QApplication, QLineEdit, QCheckBox, QSpinBox, QComboBox
-
-from PyQt5.QtTest import QTest
+from PyQt5.QtCore import QSettings
+from PyQt5.QtWidgets import QApplication
+# from PyQt5.QtTest import QTest
 
 
 from structjour.view.dailycontrol import DailyControl
-from structjour.journalfiles import JournalFiles
-from structjour.definetrades import DefineTrades
-
-from structjour.statements.ibstatementdb import StatementDB
-from structjour.statements.ibstatement import IbStatement
-
-from structjour.view.layoutforms import LayoutForms
-from structjour.view.sumcontrol import SumControl
-
-
-
-# pylint: disable = C0103
 
 app = QApplication(sys.argv)
 
+
 class TestDailyCtrl(TestCase):
-    '''Test the db calls and the user interaction stuff'''
+    '''
+    Test the db calls and the user interaction stuff. The DailyNotes form has the daily note- saved in db,
+    a daily summary table, generated from the summary form  and daily trades table, not saved
+    '''
 
     def __init__(self, *args, **kwargs):
         super(TestDailyCtrl, self).__init__(*args, **kwargs)
 
         self.testdb = 'test/testdb.sqlite'
         self.apiset = QSettings('zero_substance/stockapi', 'structjour')
-        self.realdb = self.apiset.value('dbsqlite') 
+        self.realdb = self.apiset.value('dbsqlite')
 
+    def test_DailyControl(self):
+        '''
+        Test class creation using a test database. Test that we have set the db
+        '''
+
+        dc = DailyControl(pd.Timestamp.now(), self.testdb)
+        self.assertEqual(self.testdb, dc.db)
 
     def setUp(self):
-
-        ddiirr = os.path.dirname(__file__)
-        os.chdir(os.path.realpath(ddiirr))
-        os.chdir(os.path.realpath('../'))
-        self.conn = sqlite3.connect(self.testdb)
-        self.inputtype = 'IB_HTML' 
-
-        indir = 'data/'
-        f2 = 'ActivityStatement.20190313_PL.html'
-        f1 = 'ActivityStatement.20190404.html'
-        theDate = pd.Timestamp('2019-04-04')
-
-        jf = JournalFiles(indir=indir, infile=f1, theDate=theDate, inputType='IB_HTML', mydevel=False)
-
-
-        jf.inputType = 'IB_HTML'
-        # statement = Statement_IBActivity(jf)
-        # df = statement.getTrades_IBActivity(jf.inpathfile)
-
-        ibs = IbStatement(db=self.testdb)
-        ibdb = StatementDB(self.testdb)
-        ibdb.reinitializeTradeTables()
-        ibs.openIBStatementHtml(jf.inpathfile)
-        df2 = ibdb.getStatement(theDate)
-        if df2.empty:
-            sdate = theDate.strftime('%Y-%m-%d')
-            msg = f'In test_dailycontrol.setup: Error: found no trades in db for {sdate}'
-            self.assertTrue(not df2.empty, msg)
-        
-        tu = DefineTrades(jf.inputType)
-        dframe, ldf = tu.processDBTrades(df2)
-        self.df = dframe
-        sc = SumControl()
-        lf = LayoutForms(sc, jf, dframe)
-        # lf.pickleitnow()
-        tradeSummaries = lf.runTtoSummaries(ldf)
-        self.ts = lf.ts
-        ibdb.addTradeSummaries(tradeSummaries, ldf)
+        pass
 
     def tearDown(self):
-        self.apiset.setValue('dbsqlite', self.realdb)
+        pass
 
-    def test_createTable(self):
-        cur = self.conn.cursor()
+    def test_drop_createTable(self):
+        '''Test the creation and drop of the daily notes table'''
+        dc = DailyControl(pd.Timestamp.now(), self.testdb)
 
-        cur.execute(''' DROP TABLE IF EXISTS daily_notes''')
-        self.conn.commit()
+        con = sqlite3.connect(self.testdb)
+        mycur = con.cursor()
 
-        daDate = pd.Timestamp('2021-06-06')
-        dc = DailyControl(daDate, db=self.testdb)
-        dc.runDialog(self.df, tradeSum=self.ts)
-        dc.createTable()
-        self.conn.commit()
-        zilch = dc.getNote()
-        # If we are still here, no exception, the table exists
-        self.assertIs(zilch, None)
-
-    def test_dropTable(self):
-        daDate = pd.Timestamp('2021-06-06')
-        dc = DailyControl(daDate, db=self.testdb)
-        dc.runDialog(self.df, tradeSum=self.ts)
-        dc.createTable()
-        self.conn.commit()
-        zilch = dc.getNote()
         dc.dropTable()
-        # self.assertRaises
-        self.assertRaises(sqlite3.OperationalError, dc.getNote)
+        mycur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
+        available_tables = [x[0] for x in mycur.fetchall()]
+        self.assertNotIn("daily_notes", available_tables)
+
+        dc.createTable()
+        mycur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
+        available_tables = [x[0] for x in mycur.fetchall()]
+        self.assertIn("daily_notes", available_tables)
+
+        dc.dropTable()
+        mycur.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
+        available_tables = [x[0] for x in mycur.fetchall()]
+        self.assertNotIn("daily_notes", available_tables)
 
     def test_commitNote(self):
+        '''
+        Tests commitNote with an argument for note for creating new note
+        '''
         daDate = pd.Timestamp('2021-06-06')
         dc = DailyControl(daDate, db=self.testdb)
-        dc.runDialog(self.df, tradeSum=self.ts)
+        # dc.runDialog(self.df, tradeSum=self.ts)
         dc.dropTable()
         dc.createTable()
         note = '''Twas all hallow's  eve and all throug the grove
                   jabberwock trounced his thro boro-dove'''
-        dc.ui.dailyNotes.setText(note)
-        dc.commitNote()
+        # dc.ui.dailyNotes.setText(note)
+        dc.commitNote(note=note)
         xnote = dc.getNote()
         self.assertEqual(note, xnote)
 
-        updatenote = '''Twas all hallow's  eve and all throug the grove
-                        jabberwock trounced his thro boro-dove.
-                        The Jack-lanterns sat on porch all alight,
-                        the hopes of the ghouls was thick in the night.'''
-        dc.ui.dailyNotes.setText(updatenote)
-        dc.commitNote()
+    def test_commitNote_update(self):
+        '''
+        Tests commitNote with an argument for note for creating new note
+        '''
+        daDate = pd.Timestamp('2021-06-06')
+        dc = DailyControl(daDate, db=self.testdb)
+        # dc.runDialog(self.df, tradeSum=self.ts)
+        dc.dropTable()
+        dc.createTable()
+        note = '''Twas all hallow's  eve and all throug the grove
+                  jabberwock trounced his thro boro-dove'''
+        dc.commitNote(note=note)
+        note = '''Forget all that'''
+        dc.commitNote(note=note)
         xnote = dc.getNote()
-        self.assertEqual(updatenote, xnote)
+        self.assertEqual(note, xnote)
 
     def test_getnote(self):
-        daDate = pd.Timestamp('2021-06-06')
-        dc = DailyControl(daDate, db=self.testdb)
-        dc.runDialog(self.df, tradeSum = self.ts)
-        dc.dropTable()
-        dc.createTable()
-        note = '''Twas all hallow's  eve and all throug the grove
-                  jabberwock trounced his thro boro-dove'''
-        dc.ui.dailyNotes.setText(note)
-        dc.commitNote()
-        xnote = dc.getNote()
-        self.assertEqual(note, xnote)
+        pass  # test is covered in commitNote tests
 
     def test_populateNotes(self):
-        daDate = pd.Timestamp('2021-06-06')
-        dc = DailyControl(daDate, db=self.testdb)
-        dc.runDialog(self.df, tradeSum = self.ts)
-        dc.dropTable()
-        dc.createTable()
-        dc.populateNotes()
-        note = '''Twas all hallow's  eve and all throug the grove
-                  jabberwock trounced his thro boro-dove'''
-        dc.setNote(note)
-        widgnote = dc.ui.dailyNotes.toPlainText()
-        self.assertTrue(not widgnote)
-        dc.populateNotes()
-        widgnote = dc.ui.dailyNotes.toPlainText()
-        self.assertEqual(widgnote, note)
+        pass
 
     def test_populateStuff(self):
-        '''Test populateS (modelS), populateM (modelM) and the pandas modelT'''
-        daDate = pd.Timestamp('2021-06-06')
-        dc = DailyControl(daDate, db=self.testdb)
-        dc.runDialog(self.df, tradeSum = self.ts)
-        headers = ['Daily P / L Summary', 'Live Total', 'Sim Total', 'Highest Profit',
-                   'Largest Loss', 'Average Win', 'Average Loss']
-        for i, head in enumerate(headers):
-            self.assertEqual(dc.modelS.item(i, 0).text(), head)
-
-        headers = ['Name', 'PnL', 'Lost Plays', 'Mistake or pertinent feature of trade']
-        for i, head in enumerate(headers):
-            self.assertEqual(dc.modelM.item(1, i).text(), head)
-
-        for i, head in enumerate(list(dc.modelT._df.columns)):
-            self.assertEqual(head, dc.modelT.headerData(i, Qt.Horizontal))
+        pass
 
     def test_populateM(self):
-        daDate = pd.Timestamp('2021-06-06')
-        dc = DailyControl(daDate, db=self.testdb)
-        dc.runDialog(self.df, tradeSum = self.ts)
-        headers = ['Name', 'PnL', 'Lost Plays', 'Mistake or pertinent feature of trade']
-        for i, head in enumerate(headers):
-            self.assertEqual(dc.modelM.item(1, i).text(), head)
+        pass
+
 
 def main():
     unittest.main()
 
+
 def notmain():
     t = TestDailyCtrl()
-    t.setUp()
+    # t.test_DailyControl()
+    # t.test_drop_createTable()
+    # t.test_commitNote()
+    t.test_commitNote_update()
+    # t.setUp()
 
 
 if __name__ == '__main__':
-    main()
-    # notmain()
+    # main()
+    notmain()
---
+++ test/test_dailycontrol_system.py
@@ -0,0 +1,212 @@
+# Structjour -- a daily trade review helper
+# Copyright (C) 2019 Zero Substance Trading
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+'''
+Test the module dailycontrol
+
+Created on May 22, 2019
+
+@author: Mike Petersen
+'''
+import os
+import sys
+import unittest
+from unittest import TestCase
+
+import pandas as pd
+import sqlite3
+
+# https://stackoverflow.com/questions/57426219/how-to-import-qtwebenginewidgets-after-qapplication-has-been-created
+# thanks to ekhumoro.
+# Strangely the import error occurs using (before installing webengine_hack here):
+# python -m unittest discover
+# But not when using from the test directory:
+# python -m unittest test_dailycontrol
+# or any other local run
+# def webengine_hack():
+#     from PyQt5 import QtWidgets
+#     app = QtWidgets.QApplication.instance()
+#     if app is not None:
+#         import sip
+#         app.quit()
+#         sip.delete(app)
+#     import sys
+#     from PyQt5 import QtCore, QtWebEngineWidgets
+#     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
+#     app = QtWidgets.qApp = QtWidgets.QApplication(sys.argv)
+#     return app
+
+# try:
+#     # just for testing
+#     from PyQt5 import QtWidgets
+#     app = QtWidgets.QApplication([''])
+#     from PyQt5 import QtWebEngineWidgets
+# except ImportError as exception:
+#     logging.info('\nRetrying webengine import...')
+#     app = webengine_hack()
+#     from PyQt5 import QtWebEngineWidgets
+
+# from PyQt5 import QtCore, QtWebEngineWidgets
+from PyQt5.QtCore import Qt, QSettings
+from PyQt5.QtWidgets import QApplication
+
+# from PyQt5.QtTest import QTest
+from structjour.view.dailycontrol import DailyControl
+from structjour.journalfiles import JournalFiles
+from structjour.definetrades import DefineTrades
+
+from structjour.statements.ibstatementdb import StatementDB
+from structjour.statements.ibstatement import IbStatement
+
+from structjour.view.layoutforms import LayoutForms
+from structjour.view.sumcontrol import SumControl
+
+app = QApplication(sys.argv)
+
+
+class TestDailyCtrl(TestCase):
+    '''Test sytem interaction.'''
+
+    def __init__(self, *args, **kwargs):
+        super(TestDailyCtrl, self).__init__(*args, **kwargs)
+
+        self.testdb = 'test/testdb.sqlite'
+        self.apiset = QSettings('zero_substance/stockapi', 'structjour')
+        self.realdb = self.apiset.value('dbsqlite')
+
+        # Test file has one month of trades including some on theDate
+        self.f1 = 'flex.369463.ActivityFlexMonth.20191008.20191106.csv'
+        self.theDate = pd.Timestamp('2019-10-16')
+
+    def setUp(self):
+
+        ddiirr = os.path.dirname(__file__)
+        os.chdir(os.path.realpath(ddiirr))
+        os.chdir(os.path.realpath('../'))
+        self.conn = sqlite3.connect(self.testdb)
+        self.inputtype = 'IB_CSV'
+
+        indir = 'data/'
+        # f2 = 'ActivityStatement.20190313_PL.html'
+
+        jf = JournalFiles(indir=indir, infile=self.f1, theDate=self.theDate, inputType='IB_CSV', mydevel=False)
+
+        jf.inputType = 'IB_CSV'
+        # statement = Statement_IBActivity(jf)
+        # df = statement.getTrades_IBActivity(jf.inpathfile)
+
+        ibs = IbStatement(db=self.testdb)
+        ibdb = StatementDB(self.testdb)
+        ibdb.reinitializeTradeTables()
+        ibs.openIBStatementCSV(jf.inpathfile)
+        df2 = ibdb.getStatement(self.theDate)
+        if df2.empty:
+            sdate = self.theDate.strftime('%Y-%m-%d')
+            msg = f'In test_dailycontrol.setup: Error: found no trades in db for {sdate}'
+            self.assertTrue(not df2.empty, msg)
+
+        tu = DefineTrades(jf.inputType)
+        dframe, ldf = tu.processDBTrades(df2)
+        self.df = dframe
+        sc = SumControl()
+        lf = LayoutForms(sc, jf, dframe)
+        # lf.pickleitnow()
+        tradeSummaries = lf.runTtoSummaries(ldf)
+        self.ts = lf.ts
+        ibdb.addTradeSummaries(tradeSummaries, ldf)
+
+    def tearDown(self):
+        # just in case the testdb was written to settings
+        self.apiset.setValue('dbsqlite', self.realdb)
+
+    def test_commitNote(self):
+        daDate = pd.Timestamp('2021-06-06')
+        dc = DailyControl(daDate, db=self.testdb)
+        dc.runDialog(self.df, tradeSum=self.ts)
+        dc.dropTable()
+        dc.createTable()
+        note = '''Twas all hallow's  eve and all throug the grove
+                  jabberwock trounced his thro boro-dove'''
+        dc.ui.dailyNotes.setText(note)
+        dc.commitNote()
+        xnote = dc.getNote()
+        self.assertEqual(note, xnote)
+
+        updatenote = '''Twas all hallow's  eve and all throug the grove
+                        jabberwock trounced his thro boro-dove.
+                        The Jack-lanterns sat on porch all alight,
+                        the hopes of the ghouls was thick in the night.'''
+        dc.ui.dailyNotes.setText(updatenote)
+        dc.commitNote()
+        xnote = dc.getNote()
+        self.assertEqual(updatenote, xnote)
+
+    def test_populateNotes(self):
+        daDate = pd.Timestamp('2021-06-06')
+        dc = DailyControl(daDate, db=self.testdb)
+        dc.runDialog(self.df, tradeSum=self.ts)
+        dc.dropTable()
+        dc.createTable()
+        dc.populateNotes()
+        note = '''Twas all hallow's  eve and all throug the grove
+                  jabberwock trounced his thro boro-dove'''
+        dc.setNote(note)
+        widgnote = dc.ui.dailyNotes.toPlainText()
+        self.assertTrue(not widgnote)
+        dc.populateNotes()
+        widgnote = dc.ui.dailyNotes.toPlainText()
+        self.assertEqual(widgnote, note)
+
+    def test_populateStuff(self):
+        '''Test populateS (modelS), populateM (modelM) and the pandas modelT'''
+        daDate = pd.Timestamp('2021-06-06')
+        dc = DailyControl(daDate, db=self.testdb)
+        dc.runDialog(self.df, tradeSum=self.ts)
+        headers = ['Daily P / L Summary', 'Live Total', 'Sim Total', 'Highest Profit',
+                   'Largest Loss', 'Average Win', 'Average Loss']
+        for i, head in enumerate(headers):
+            self.assertEqual(dc.modelS.item(i, 0).text(), head)
+
+        headers = ['Name', 'PnL', 'Lost Plays', 'Mistake or pertinent feature of trade']
+        for i, head in enumerate(headers):
+            self.assertEqual(dc.modelM.item(1, i).text(), head)
+
+        for i, head in enumerate(list(dc.modelT._df.columns)):
+            self.assertEqual(head, dc.modelT.headerData(i, Qt.Horizontal))
+
+    def test_populateM(self):
+        daDate = pd.Timestamp('2021-06-06')
+        dc = DailyControl(daDate, db=self.testdb)
+        dc.runDialog(self.df, tradeSum=self.ts)
+        headers = ['Name', 'PnL', 'Lost Plays', 'Mistake or pertinent feature of trade']
+        for i, head in enumerate(headers):
+            self.assertEqual(dc.modelM.item(1, i).text(), head)
+
+
+def main():
+    unittest.main()
+
+
+def notmain():
+    t = TestDailyCtrl()
+    t.setUp()
+    t.commitNote()
+    t.tearDown()
+
+
+if __name__ == '__main__':
+    notmain()
+    # main()
